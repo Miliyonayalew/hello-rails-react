7ca36a38a7dd1973ec16b68337d0486199cc02e1
Miliyonayalew <miliayalew@gmail.com>
2023-02-21 01:22:06 +0300

    Create action, reducer and store

---
+++ app/javascript/redux/reducer/greetings.js
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchGreeting = createAsyncThunk('greeting/fetchGreeting', () => {
+  return axios.get('http://localhost:3000/greetings')
+    .then((response) => response.data)
+});
+
+
+const initialState = {
+  message: [],
+  status: 'idle',
+}
+
+const greetingSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreeting.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchGreeting.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.message = action.payload;
+      })
+  },
+});
+
+export const { greetingReducer } = greetingSlice.actions;
+
+export default greetingSlice.reducer;
