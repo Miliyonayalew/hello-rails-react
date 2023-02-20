d6fc040a26edb8d25ebee3f8d25f94ff8841f2c2
Miliyonayalew <miliayalew@gmail.com>
2023-02-21 01:23:17 +0300

    Add greeting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello, Rails React!")
+Greeting.create(message: "Hello from Milyon!")
+Greeting.create(message: "Good morning from Milyon!")
+Greeting.create(message: "Good afternoon from Milyon!")
+Greeting.create(message: "Good evening from Milyon!")
